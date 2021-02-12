1d9b87fe3b0bf7e2ff8c8c2119882f9cf7a04e2c
Lucas Marandat <lucas.mrdt@gmail.com>
2021-02-12 13:44:02 +0100

    fix request changes

--- src/main/java/iialib/games/algs/algorithms/AlphaBeta.java
+++ src/main/java/iialib/games/algs/algorithms/AlphaBeta.java
@@ -7,10 +7,19 @@ import iialib.games.algs.IHeuristic;
 import iialib.games.model.IBoard;
 import iialib.games.model.IMove;
 import iialib.games.model.IRole;
-import iialib.games.model.Tuple;
 
 public class AlphaBeta<Move extends IMove, Role extends IRole, Board extends IBoard<Move, Role, Board>>
 		implements GameAlgorithm<Move, Role, Board> {
+
+	private class ABResult {
+		public int best_alpha;
+		public Move best_move;
+
+		public ABResult(int best_alpha, Move best_move) {
+			this.best_alpha = best_alpha;
+			this.best_move = best_move;
+		}
+	}
 
 	// Constants
 	/**
@@ -72,9 +81,8 @@ public class AlphaBeta<Move extends IMove, Role extends IRole, Board extends IBo
 		System.out.println("[AlphaBeta]");
 
 		int p = playerRole == this.playerMinRole ? -1 : 1;
-		Tuple<Integer, Move> result = this.negAlphaBeta(board, playerRole, 0, IHeuristic.MIN_VALUE,
-				IHeuristic.MAX_VALUE, p);
-		return result.second;
+		ABResult result = this.negAlphaBeta(board, playerRole, 0, IHeuristic.MIN_VALUE, IHeuristic.MAX_VALUE, p);
+		return result.best_move;
 	}
 
 	/*
@@ -92,11 +100,11 @@ public class AlphaBeta<Move extends IMove, Role extends IRole, Board extends IBo
 		return playerRole == this.playerMinRole ? this.playerMaxRole : this.playerMinRole;
 	}
 
-	private Tuple<Integer, Move> negAlphaBeta(Board board, Role playerRole, int depth, int alpha, int beta, int p) {
+	private ABResult negAlphaBeta(Board board, Role playerRole, int depth, int alpha, int beta, int p) {
 		++this.nbNodes;
 		if (depth >= this.depthMax || board.isGameOver()) {
 			++this.nbLeaves;
-			return new Tuple<>(p * this.h.eval(board, playerRole), null);
+			return new ABResult(p * this.h.eval(board, playerRole), null);
 		}
 
 		ArrayList<Move> possibleMoves = board.possibleMoves(playerRole);
@@ -104,15 +112,15 @@ public class AlphaBeta<Move extends IMove, Role extends IRole, Board extends IBo
 		Move best_move = null;
 		for (Move move : possibleMoves) {
 			Board newBoard = board.play(move, playerRole);
-			Tuple<Integer, Move> result = this.negAlphaBeta(newBoard, opponent, depth + 1, -beta, -alpha, -p);
-			if (-result.first > alpha) {
-				alpha = -result.first;
+			ABResult result = this.negAlphaBeta(newBoard, opponent, depth + 1, -beta, -alpha, -p);
+			if (-result.best_alpha > alpha) {
+				alpha = -result.best_alpha;
 				best_move = move;
 			}
 			if (alpha >= beta) {
-				return new Tuple<>(beta, best_move);
+				return new ABResult(beta, best_move);
 			}
 		}
-		return new Tuple<>(alpha, best_move);
+		return new ABResult(alpha, best_move);
 	}
 }
