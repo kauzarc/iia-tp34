e4aa5a3c8389f293bd129fa333120c65cfce8c79
kauzarc <45573975+kauzarc@users.noreply.github.com>
2021-02-14 20:10:05 +0100

    negAlphaBeta

--- src/main/java/games/awale/AwaleGame.java
+++ src/main/java/games/awale/AwaleGame.java
@@ -5,36 +5,36 @@ import java.util.ArrayList;
 import iialib.games.algs.AIPlayer;
 import iialib.games.algs.AbstractGame;
 import iialib.games.algs.GameAlgorithm;
-import iialib.games.algs.algorithms.AlphaBeta;
+import iialib.games.algs.algorithms.NegAlphaBeta;
 
 public class AwaleGame extends AbstractGame<AwaleMove, AwaleRole, AwaleBoard> {
 
-    public AwaleGame(ArrayList<AIPlayer<AwaleMove, AwaleRole, AwaleBoard>> players, AwaleBoard initialBoard) {
-        super(players, initialBoard);
-    }
+        public AwaleGame(ArrayList<AIPlayer<AwaleMove, AwaleRole, AwaleBoard>> players, AwaleBoard initialBoard) {
+                super(players, initialBoard);
+        }
 
-    public static void main(String[] args) {
-        AwaleRole role1 = AwaleRole.PLAYER1;
-        AwaleRole role2 = AwaleRole.PLAYER2;
+        public static void main(String[] args) {
+                AwaleRole role1 = AwaleRole.PLAYER1;
+                AwaleRole role2 = AwaleRole.PLAYER2;
 
-        GameAlgorithm<AwaleMove, AwaleRole, AwaleBoard> alg1 = new AlphaBeta<AwaleMove, AwaleRole, AwaleBoard>(role1,
-                role2, AwaleHeuristics.h1, 10);
-        GameAlgorithm<AwaleMove, AwaleRole, AwaleBoard> alg2 = new AlphaBeta<AwaleMove, AwaleRole, AwaleBoard>(role2,
-                role1, AwaleHeuristics.h1, 10);
+                GameAlgorithm<AwaleMove, AwaleRole, AwaleBoard> alg1 = new NegAlphaBeta<AwaleMove, AwaleRole, AwaleBoard>(
+                                role1, role2, AwaleHeuristics.h1, 14);
+                GameAlgorithm<AwaleMove, AwaleRole, AwaleBoard> alg2 = new NegAlphaBeta<AwaleMove, AwaleRole, AwaleBoard>(
+                                role2, role1, AwaleHeuristics.h1, 14);
 
-        AIPlayer<AwaleMove, AwaleRole, AwaleBoard> player1 = new AIPlayer<AwaleMove, AwaleRole, AwaleBoard>(role1,
-                alg1);
-        AIPlayer<AwaleMove, AwaleRole, AwaleBoard> player2 = new AIPlayer<AwaleMove, AwaleRole, AwaleBoard>(role2,
-                alg2);
+                AIPlayer<AwaleMove, AwaleRole, AwaleBoard> player1 = new AIPlayer<AwaleMove, AwaleRole, AwaleBoard>(
+                                role1, alg1);
+                AIPlayer<AwaleMove, AwaleRole, AwaleBoard> player2 = new AIPlayer<AwaleMove, AwaleRole, AwaleBoard>(
+                                role2, alg2);
 
-        ArrayList<AIPlayer<AwaleMove, AwaleRole, AwaleBoard>> players = new ArrayList<AIPlayer<AwaleMove, AwaleRole, AwaleBoard>>();
+                ArrayList<AIPlayer<AwaleMove, AwaleRole, AwaleBoard>> players = new ArrayList<AIPlayer<AwaleMove, AwaleRole, AwaleBoard>>();
 
-        players.add(player1);
-        players.add(player2);
+                players.add(player1);
+                players.add(player2);
 
-        AwaleBoard initialBoard = new AwaleBoard();
+                AwaleBoard initialBoard = new AwaleBoard();
 
-        AwaleGame game = new AwaleGame(players, initialBoard);
-        game.runGame();
-    }
+                AwaleGame game = new AwaleGame(players, initialBoard);
+                game.runGame();
+        }
 }
